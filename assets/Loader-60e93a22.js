de40921f6d8fb6fa140d1610c61feef47509be87
unknown <xbaranovic@stuba.sk>
2023-05-14 12:20:40 +0200

    delete all

--- assets/Loader-60e93a22.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o,c as t,p as a,d as c,a as _}from"./index-f7f9df56.js";const d={},n=e=>(a("data-v-9e4ad506"),e=e(),c(),e),i={class:"is-flex is-justify-content-center is-align-items-center"},p=n(()=>_("div",{id:"loading"},null,-1)),r=[p];function l(e,u,f,h,v,m){return o(),t("div",i,r)}const I=s(d,[["render",l],["__scopeId","data-v-9e4ad506"]]);export{I as L};
