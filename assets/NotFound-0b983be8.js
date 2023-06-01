d8ae1b6ba86d71e4f2446009c9bd23d88720e905
unknown <xbaranovic@stuba.sk>
2023-06-01 13:34:01 +0200

    delete all files

--- assets/NotFound-0b983be8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./unauthorized-c0a8db7d.js";import{_ as s,c as o,o as c,a as t}from"./index-a675c8ae.js";const n={},_={class:"has-text-centered"},a=t("h1",{class:"title"},"Takáto stránka u nás bohužiaľ neexistuje",-1),r=t("img",{src:e},null,-1),i=[a,r];function d(l,h){return c(),o("div",_,i)}const f=s(n,[["render",d]]);export{f as default};
